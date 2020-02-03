3536666da6fb89322bb990d90021ff33649d2a9b
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-02-03 13:21:16 +0100

    api url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
 	production: true,
 	isMockEnabled: true, // You have to switch this, when your real back-end is done
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
-	BASE_URL: 'http://159.89.85.116/v1'
+	BASE_URL: 'https://api.parableus.ng/v1'
 };
